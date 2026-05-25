bcde2f55fd861abc2780a53a71f9bf352253b88b
lvemail <11128730@qq.com>
2026-05-25 09:51:08 +0800

    1

--- src/apps/com.moutai.mall.ts
+++ src/apps/com.moutai.mall.ts
@@ -6,8 +6,8 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '定时抢购',
-      desc: '持续点击"20:00投放"按钮直到消失',
+      name: '定时抢购500ml',
+      desc: '持续点击"立即购买"按钮直到消失',
       activityIds: '.webview.view.WebViewActivity',
       enable: true,
       matchTime: 300000, // 10秒内持续检测
@@ -16,6 +16,22 @@ export default defineGkdApp({
       rules: [
         {
           matches: '[text="立即购买"][clickable=true]',
+          action: 'click',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '定时抢购1L',
+      desc: '持续点击"立即购买"按钮直到消失',
+      activityIds: '.webview.view.WebViewActivity',
+      enable: true,
+      matchTime: 300000, // 10秒内持续检测
+      resetMatch: 'activity', // 离开页面后重置
+      // 移除 actionMaximum，允许无限次点击
+      rules: [
+        {
+          matches: '[text="20:09投放"][clickable=true]',
           action: 'click',
         },
       ],
